d9c5fbd78280a5328ceffe91283774a2abcd2136
bobo <gobobo3@live.com>
2022-09-27 17:42:56 +0800

    值传递和引用传递，深浅拷贝

--- app/day4-oop/oop.go
+++ app/day4-oop/oop.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type Voice interface {
-	cry()  string
+	cry() string
 	laught() string
 	interested() string
 }
@@ -11,27 +11,25 @@ type Voice interface {
 type Profile interface {
 	height() float32
 	weight() float32
-	gender() string 
+	gender() string
 }
 
 type People struct {
-    name string
-    income []int
-	id  map[string]string
+	name   string
+	income []int
+	id     map[string]string
 }
 
 type Animal struct {
 	name string
-	id map[string]string
+	id   map[string]string
 }
 
 func (aoao *People) cry() string {
-  fmt.Printf("this is people cry: %v\n",aoao.name)
-  s := ""
-  return s 
+	fmt.Printf("this is people cry: %v\n", aoao.name)
+	s := ""
+	return s
 }
-
-
 
 func main() {
 
